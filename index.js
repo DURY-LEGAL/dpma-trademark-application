f9c9b923ff15c9aeaf51e871502ffeb7477003b8
Slarti @ DURY LEGAL <slarti@dury.de>
2026-05-03 22:38:02 +0200

    Initial commit: index.js

---
+++ index.js
@@ -0,0 +1,3 @@
+// dpma-trademark-application - DPMA trademark application helper - German Patent and Trademark Office integration. Powered by DURY LEGAL.
+// Coming soon. Visit https://www.dury.de for more information.
+module.exports = {};
